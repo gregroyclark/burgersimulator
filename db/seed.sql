992c5c3c578e4dbd1762df59ab675c09644b3a80
peacefrog <gregroyclark@gmail.com>
2020-05-01 13:21:57 -0500

    populates MySQL burgers_db with seed data

--- db/seed.sql
+++ db/seed.sql
@@ -1,8 +1,26 @@
 INSERT INTO burgers (
-  id,
   burger_name,
   eaten
 )
 VALUES (
-    
+  "krabby patty",
+  false  
+);
+
+INSERT INTO burgers (
+  burger_name,
+  eaten
 )
+VALUES (
+  "bison burger",
+  false
+);
+
+INSERT INTO burgers (
+  burger_name,
+  eaten
+)
+VALUES (
+  "double double",
+  false
+);
