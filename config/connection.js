62c499f9a1682a8a0052460ea0da66360bd82389
peacefrog <gregroyclark@gmail.com>
2020-05-01 09:07:03 -0500

    creates connection

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: process.env.host,
+  port: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
