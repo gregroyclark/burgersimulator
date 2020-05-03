1c7fe2940f841356fb93a04becb82d92e223f3b6
peacefrog <gregroyclark@gmail.com>
2020-05-03 15:13:41 -0500

    changes variables to var

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-const routes = require("./controllers/burgersController.js");
+var routes = require("./controllers/burgersController.js");
 
 app.use(routes);
 
