12e6721c1e76276937dfe577fe38f3069dbef7d3
peacefrog <gregroyclark@gmail.com>
2020-05-02 20:10:33 -0500

    changes variables to ES6 server.js

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require("express");
 
 const PORT = process.env.PORT || 8080;
 
-var app = express();
+const app = express();
 
 app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
