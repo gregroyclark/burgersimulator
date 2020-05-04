16057df4d99897ab044116cdf6251c498025fc5e
peacefrog <gregroyclark@gmail.com>
2020-05-04 10:45:24 -0500

    makes database functions more specific

--- models/burger.js
+++ models/burger.js
@@ -2,21 +2,24 @@ var orm = require("../config/orm.js");
 
 //database functions
 var burger = {
-    all: function(cb) {
-      orm.all("burgers", function(res) {
+    selectAll: function(cb) {
+      orm.selectAll("burgers", function(res) {
         cb(res);
       });
     },
-    create: function(cols, vals, cb) {
-      orm.create("burgers", cols, vals, function(res) {
+
+    createOne: function(cols, vals, cb) {
+      orm.createOne("burgers", cols, vals, function(res) {
         cb(res);
       });
     },
-    update: function(objColVals, condition, cb) {
-      orm.update("burgers", objColVals, condition, function(res) {
+
+    updateOne: function(objColVals, condition, cb) {
+      orm.updateOne("burgers", objColVals, condition, function(res) {
         cb(res);
       });
     },
+
     delete: function(condition, cb) {
       orm.delete("burger", condition, function(res) {
         cb(res);
