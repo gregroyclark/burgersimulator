d5665083c3913797af333faf98d48333e7c258f9
peacefrog <gregroyclark@gmail.com>
2020-05-03 19:04:25 -0500

    erases model object from orm.js, switches PORT to 8080

--- config/orm.js
+++ config/orm.js
@@ -1,30 +1,5 @@
 var connection = require("../config/connection.js");
 
-// var burger = {
-//     all: function(cb) {
-//       orm.all("burgers", function(res) {
-//         cb(res);
-//       });
-//     },
-//     create: function(cols, vals, cb) {
-//       orm.create("burgers", cols, vals, function(res) {
-//         cb(res);
-//       });
-//     },
-//     update: function(objColVals, condition, cb) {
-//       orm.update("burgers", objColVals, condition, function(res) {
-//         cb(res);
-//       });
-//     },
-//     delete: function(condition, cb) {
-//       orm.delete("burger", condition, function(res) {
-//         cb(res);
-//       });
-//     }
-//   };
-
-//   module.exports = burger;
-
 function printQuestionMarks(num) {
     var arr = [];
   
