19d2a0ab660f2797bd4ebfb96b3887b0f301d657
peacefrog <gregroyclark@gmail.com>
2020-05-04 08:22:06 -0500

    changes router order to match CRUD flow

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -4,16 +4,6 @@ var router = express.Router();
 
 var burger = require("../models/burger.js");
 
-router.get("/", function(req, res) {
-   burger.all(function(data) {
-     let hbsObject = {
-       burgers: data  
-     };
-     console.log(hbsObject);
-     res.render("index", hbsObject);
-   });
-});
-
 router.post("/api/burgers", function(req, res) {
   burger.create([
     "name", "eaten"
@@ -21,6 +11,16 @@ router.post("/api/burgers", function(req, res) {
     req.body.name, req.body.eaten 
   ], function(result){
     res.json({ id: result.insertId });
+  });
+});
+
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    let hbsObject = {
+      burgers: data  
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
   });
 });
 
