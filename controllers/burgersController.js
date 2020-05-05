5c5e183b91e58a4eeb9a9eb186af8f9cd9a9f7ce
peacefrog <gregroyclark@gmail.com>
2020-05-05 08:58:15 -0500

    trims page breaks from first 3 variables, changes "orm" to "burger"

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,11 +1,9 @@
 var express = require("express");
-
 var router = express.Router();
-
 var burger = require("../models/burger.js");
 
 router.post("/api/burgers", function(req, res) {
-  orm.create([
+  burger.create([
     "name", "eaten"
   ], [
     req.body.name, req.body.eaten 
@@ -15,7 +13,7 @@ router.post("/api/burgers", function(req, res) {
 });
 
 router.get("/", function(req, res) {
-  orm.selectAll(function(data) {
+  burger.all(function(data) {
     var hbsObject = {
       burgers: data  
     };
@@ -29,7 +27,7 @@ router.put("/api/burgers/:id", function(req, res){
 
   console.log("condition", condition);
 
-  orm.updateOne({
+  burger.update({
     eaten: req.body.eaten
   }, condition, function(result){
     if (result.changedRows == 0){
@@ -43,7 +41,7 @@ router.put("/api/burgers/:id", function(req, res){
 router.delete("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  orm.delete(condition, function(result) {
+  burger.delete(condition, function(result) {
     if (result.affectedRows == 0){
       return res.status(404).end();
     } else {
